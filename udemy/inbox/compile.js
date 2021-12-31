f4397efc75d49c4bf8f83dc0d4f8a540ef4b8768
thebrard <thomas.hebrard134@gmail.com>
2021-12-31 18:09:23 +0100

    compile

---
+++ udemy/inbox/compile.js
@@ -0,0 +1,6 @@
+const path = require("path");
+const fs = require("fs");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+compile.log(solc.compile(source, 1));
